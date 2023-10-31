501e345de9bfb84e3f93a3718983f13037fd220e
iasraful <iasraful321@gmail.com>
2023-10-31 20:27:17 +0600

    Completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="social">
+      <a href="#">
+        <img src="/facebook.png" alt="Facebook Icon" />
+      </a>
+      <a href="#">
+        <img src="/instagram.png" alt="Instageam" />
+      </a>
+      <a href="#">
+        <img src="/youtube.png" alt="Youtube" />
+      </a>
+      <a href="#">
+        <img src="/dribbble.png" alt="Dribble" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
